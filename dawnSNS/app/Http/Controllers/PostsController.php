4660560e4dd1a71fbce907de833ba4e165b31229
kosuke-kashiwa <kosuke.kashiwa0623@gmail.com>
2021-04-17 15:46:37 +0900

    タイムゾーンの変更

--- dawnSNS/app/Http/Controllers/PostsController.php
+++ dawnSNS/app/Http/Controllers/PostsController.php
@@ -11,20 +11,16 @@ use App\User;
 class PostsController extends Controller
 {
     //
-    
     public function index(User $user){
         $auths = Auth::user();
         // dd($auths);
         $list = Post::get();
-            //   ->orderBy('created_at','desc');
-
         return view('posts.index',['list' => $list]);
     }
 
     public function create(Request $request)
     { 
         $post = new Post();
-       
         $post->user_id = Auth::user()->id;
         // dd($post);
         $post->posts = $request->input('newPost');
